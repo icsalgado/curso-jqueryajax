b2a560fd03b0283fd722fec668dcaae037419b4c
icsalgado <icsalgado@icloud.com>
2021-01-13 03:32:00 -0300

    Utilizando funções com jquery e ajax

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function consultacep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep +"/json/";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            document.getElementById("logradouro").innerHTML = response.logradouro;
+            $("#bairro").html(response.bairro);
+            //alert(response.logradouro);
+        }
+    })
+}
